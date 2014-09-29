e32de858d35783082d62bdfdcdbcfab3b3d7913f
davis <davis.dimalen@gmail.com>
2014-09-30 09:50:13 +1300

    Update test

--- app.js
+++ app.js
@@ -3,32 +3,32 @@ window.addEventListener('load', function () {
 }, false);
 
 // The dynamically built HTML pages. In a real-life app, In a real-life app, use Handlerbar.js, Mustache.js or another template engine
-//var homePage =
-//    '<div>' +
-//        '<div class="header"><h1>Page Slider</h1></div>' +
-//        '<div class="scroller">' +
-//            '<ul class="list">' +
-//                '<li><a href="#page1"><strong>Build Bot</strong></a></li>' +
-//                '<li><a href="#page2"><strong>Medi Bot</strong></a></li>' +
-//                '<li><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
-//            '</ul>' +
-//        '</div>' +
-//    '</div>';
-//
-//var detailsPage =
-//        '<div>' +
-//                '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
-//                '<div class="scroller">' +
-//                '<div class="robot">' +
-//                '<img src="images/{{img}}"/>' +
-//                '<h2>{{name}}</h2>' +
-//                '<p>{{description}}</p>' +
-//                '</div>' +
-//                '</div>' +
-//                '</div>';
-//
-//window.localStorage.setItem("homePage", homePage);
-//window.localStorage.setItem("detailsPage", detailsPage);
+var homePage =
+    '<div>' +
+        '<div class="header"><h1>Page Slider</h1></div>' +
+        '<div class="scroller">' +
+            '<ul class="list">' +
+                '<li><a href="#page1"><strong>Build Bot</strong></a></li>' +
+                '<li><a href="#page2"><strong>Medi Bot</strong></a></li>' +
+                '<li><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
+            '</ul>' +
+        '</div>' +
+    '</div>';
+
+var detailsPage =
+        '<div>' +
+                '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
+                '<div class="scroller">' +
+                '<div class="robot">' +
+                '<img src="images/{{img}}"/>' +
+                '<h2>{{name}}</h2>' +
+                '<p>{{description}}</p>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+
+window.localStorage.setItem("homePage", homePage);
+window.localStorage.setItem("detailsPage", detailsPage);
 
 var slider = new PageSlider($("#container"));
 $(window).on('hashchange', route);
@@ -49,7 +49,7 @@ function route(event) {
 //        slider.slide($(page), "right");
     }
     else {
-        page = homePage;
+        page = window.localStorage.getItem("homePage");
 //        slider.slide($(homePage), "left");
     }
 
